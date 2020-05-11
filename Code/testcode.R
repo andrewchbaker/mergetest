b468ab5119a8fd7ae44adc27e3ca33251677ad66
Grant McDermott <grant.mcdermott@gmail.com>
2020-05-11 13:34:26 -0700

    Vectorise+join
    For posterity in case anyone else stumbles on this

--- Code/testcode.R
+++ Code/testcode.R
@@ -1,33 +1,11 @@
-library(tidyverse)
-library(dtplyr)
 library(data.table)
-library(pbapply)
-library(parallel)
 
-comp_test <- data.table(read_rds(here::here("Data", "comp_test.rds")))
-inst_test <- data.table(read_rds(here::here("Data", "inst_test.rds")))
+comp_test = data.table(readRDS(here::here("Data", "comp_test.rds")))
+inst_test = data.table(readRDS(here::here("Data", "inst_test.rds")))
 
-# function to find the closest filing within 6 months of the datadate
-find_inst_match <- function(i) {
-  inst_test %>% 
-    .[cusip == comp_test$cusip[i] & 
-        rdate <= comp_test$datadate[i] & 
-        comp_test$datadate[i] - rdate <= months(11)] %>% 
-    .[rdate == max(rdate), c("inst_shares")] %>% 
-    .[, `:=`(firm_id = comp_test$firm_id[i], fyear = comp_test$fyear[i])] %>% 
-    as_tibble()
-}
+setkey(comp_test, cusip)
+setkey(inst_test, cusip)
 
-# parallelize and run over all of our observations
-# set cores
-cl <- makeCluster(4)
-
-## export data and functions to the clusters
-clusterExport(cl, c("inst_test", "comp_test", "find_inst_match"))
-
-# export needed programs
-clusterEvalQ(cl, c(library(tidyverse), library(data.table)))
-
-## run the command on the clusters
-inst_data <- do.call(rbind, pblapply(cl = cl, X = 1:nrow(comp_short), FUN = find_inst_match))
-stopCluster(cl)
+inst_data = merge(inst_test, comp_test, allow.cartesian = TRUE, by = "cusip")[
+  rdate <= datadate & datadate - rdate <= months(11), .SD, by = cusip][
+    , .SD[which.max(rdate)], by = .(cusip, fyear)]
